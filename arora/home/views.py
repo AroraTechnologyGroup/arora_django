bdf1a30243e6ec12c21f6c53417e229225024cd4
Richard Hughes <richardh522@gmail.com>
2016-09-21 18:25:13 -0400

    adding remote user for login page

--- arora/home/views.py
+++ arora/home/views.py
@@ -19,8 +19,15 @@ class HomePage(APIView):
     template = r'home/main_content/main_content.html'
 
     def get(self, request, format=None):
+        try:
+            if not request.META['REMOTE_USER'].is_authenticated():
+                return redirect(reverse('home:login'))
+        except KeyError:
+            pass
+
         if not request.user.is_authenticated():
             return redirect(reverse('home:login'))
+
         resp = Response(template_name=self.template)
         resp['Cache-Control'] = 'no-cache'
         return resp
