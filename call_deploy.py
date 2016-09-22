bc970da6ef7d92f597d348b8697b7a17f5048783
Richard Hughes <richardh522@gmail.com>
2016-09-22 12:00:49 -0400

    calling new CI application on server

--- call_deploy.py
+++ call_deploy.py
@@ -1,3 +1,3 @@
 import requests
-req = requests.get("https://gisapps.aroraengineers.com:8005/git-pull")
+req = requests.get("https://gisapps.aroraengineers.com:8006/git-pull/arora-staging")
 print(req.content)
